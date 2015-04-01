29b0431aeb5189a696daa2e32e14dfae974e5b0e
Robert Lodico <rjlodico@ncsu.edu>
2015-03-31 22:59:24 -0400

    Deleted record, compilable, added Enum

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function OtakuCore() {}
 OtakuCore.Validation = require('validator');
 OtakuCore.Database = require('./lib/database');
 OtakuCore.Datatype = require('./lib/datatype');
+OtakuCore.Enum = require('./lib/enum');
 
 /* Export the module */
 xport(OtakuCore);
--- test/index.js
+++ test/index.js
@@ -1,11 +1,22 @@
 var xport = require('node-xport')(module),
-	OtakuCore = require('../');
+	OtakuCore = require('../'),
+	Enum = OtakuCore.Enum;
 
 function OtakuTest() {}
 
 OtakuTest.print = function () {
 	console.log("OtakuCore:");
 	console.log(OtakuCore);
+
+	var myEnum = new Enum("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+	console.log(myEnum.toString());
+	console.log(myEnum.toString(true));
+	console.log(myEnum.toKVString());
+
+	var myEnum2 = new Enum({ key: "Sunday", value: 99 }, "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+	console.log(myEnum2.toString());
+	console.log(myEnum2.toString(true));
+	console.log(myEnum2.toKVString());
 };
 
 console.log("Testing otaku-core");
