6ca38278a53a16440a7f78fc94b0b0b4ebe13978
Robert Lodico <rjlodico@ncsu.edu>
2015-03-31 23:59:34 -0400

    Updated some stuff

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-var xport = require('node-xport')(module);
-
-function OtakuCore() {}
-
-OtakuCore.Validation = require('validator');
-OtakuCore.Database = require('./lib/database');
-OtakuCore.Datatype = require('./lib/datatype');
-OtakuCore.Enum = require('./lib/enum');
+var xport = require('node-xport')(module),
+	OtakuCore = require('./lib/');
 
 /* Export the module */
 xport(OtakuCore);
